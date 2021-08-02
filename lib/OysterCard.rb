624dfa119360b17b5ebaa6a604789ecffbacd01b
Fiona Apps <wikipanyd@gmail.com>
2021-08-02 16:12:45 +0100

    chapter 5

--- lib/OysterCard.rb
+++ lib/OysterCard.rb
@@ -1,3 +1,12 @@
 class OysterCard
+    
+    attr_reader :balance
 
+    def initialize(balance = 0)
+      @balance = balance
+    end
+   
+    def top_up(credit)
+      @balance = balance + credit
+    end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,6 +2,21 @@ require 'OysterCard'
 
 describe OysterCard do
 
+    it 'should show balance of zero' do
+      expect(subject.balance).to eq(0)
+    end
+
+    it 'should add to the balance' do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+
 end
 
-#Had an uninitialized constant OysterCard
+# make sure it can be topped up
+# checked if top up method invoked value argument
+# balance card increase by that amount
+# 
+# i want to add money to my card
+
+# top_up
